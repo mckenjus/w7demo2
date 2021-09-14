0c6549431cbb2da80d021a05b39a68c1f565a12c
Justin McKenzie <mckenjus@gmail.com>
2021-09-14 13:19:09 -0600

    bleh1

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.get("/", (req,res) => {
     Rollbar.info(`HTML file served successfully!`);
 })
 
+function food(event) {
+    event.preventDefault();
+    const random = foodArr[Math.floor(Math.random() * foodArr.length)];
+    // document.getElementById('#result').innerhtml = foodArr[random]
+    // alert(`you should try` foodArr(randomElement))
+    alert(random, foodArr[random])
+
 const port = process.env.PORT || 4545
 
 app.listen(port, () => {
