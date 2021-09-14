1d3018af9da29defe9ec506f790869eb466b5c76
Justin McKenzie <mckenjus@gmail.com>
2021-09-14 09:33:45 -0600

    renamed files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const Rollbar = Rollbar()
+
+let Rollbar = new Rollbar({
+    accesstoken: "61ce0b145ef6404d88cc72d16ddc6750",
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
+const app = express()
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+    Rollbar.info(`HTML file served successfukky!`);
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => {
+    console.log(`all ears on ${port}!`)
+})
